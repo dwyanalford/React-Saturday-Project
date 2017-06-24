bd10c60078801a197b36009acfc45f1524b7f04b
Dwyan Alford <dwyanalford@Dwyans-MacBook-Pro.local>
2017-06-24 12:08:30 -0400

    server.js file upload

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 // Require our dependecies
 var express = require("express");
-var mongoose = require("mongoose");
-var bluebird = require("bluebird");
 var bodyParser = require("body-parser");
 var routes = require("./routes/routes");
 
 // Set up a default port, configure mongoose, configure our middleware
 var PORT = process.env.PORT || 3000;
-mongoose.Promise = bluebird;
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
