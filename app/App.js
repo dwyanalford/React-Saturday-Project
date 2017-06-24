0ca0ae5292930536f76f30a1ed96428153ecc33e
EtWham <etwham@gmail.com>
2017-06-24 13:27:06 -0400

    removing stuff

--- app/App.js
+++ app/App.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const appRoot = document.getElementById('app');
-const App = (props) => <p>Hello World</p>;
+// const App = (props) => <p>Hello World</p>;
 
-ReactDOM.render(<App/>, appRoot);
+ReactDOM.render(<App />, appRoot);
